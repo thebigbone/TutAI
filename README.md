ce3a51a0f424f6db3e0026d0d3adea48c7acddf2
thebigbone <pacman@duck.com>
2023-10-08 11:56:27 +0530

    push code

---
+++ README.md
@@ -0,0 +1,12 @@
+## how to run
+
+- install ollama
+- install requirements
+  `pip install -r requirements.txt`
+- run `flask run`
+- done
+
+## to do
+
+- make a tutorial based on a PDF and image
+- add openai support
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, render_template_string
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=["POST"])
+def result():
+    title = request.form.get("title")
+    instructions = request.form.get("instructions")
+
+    final = "generate a tutorial for the topic:" + title + \
+        " with instructions as follows: " + instructions + ". use html to write it."
+
+    url = "http://localhost:11434/api/generate"
+    data = {
+        "model": "mistral",
+        "prompt": final
+    }
+
+    response = requests.post(url, data=json.dumps(data), stream=True)
+
+    response_values = ""
+
+    for line in response.iter_lines():
+        if line:
+            line = line.rstrip()
+            response_json = json.loads(line)
+            response_value = response_json.get('response')
+            if response_value is not None:
+                response_values += response_value
+            else:
+                break
+
+    return render_template_string(response_values)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
